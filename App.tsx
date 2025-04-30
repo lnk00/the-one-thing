23c0050098b6fb56256608bb2c739318cc7ef837
Damien Dumontet <damiendumontet@192.168.68.105>
2025-04-30 18:42:56 -0300

    make title to the top and add safe area expo module

--- App.tsx
+++ App.tsx
@@ -1,25 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   Easing,
 } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const opacity = useSharedValue(0);
-  const translateY = useSharedValue(20);
+  const translateY = useSharedValue(70);
 
   useEffect(() => {
     opacity.value = withTiming(1, {
       duration: 1000,
       easing: Easing.out(Easing.ease),
     });
-    translateY.value = withTiming(0, {
+    translateY.value = withTiming(50, {
       duration: 500,
-      easing: Easing.elastic(1.2),
+      easing: Easing.elastic(),
     });
   }, [opacity, translateY]);
 
@@ -31,12 +32,12 @@ export default function App() {
   });
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Animated.Text style={[styles.title, animatedStyle]}>
         The One Thing
       </Animated.Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -44,8 +45,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffed03',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingTop: 24,
   },
   title: {
     fontSize: 32,
