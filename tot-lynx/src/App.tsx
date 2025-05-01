9e5e1f223c581b8e9aa7811d2e015bab57335526
Damien Dumontet <damiendumontet@192.168.68.105>
2025-05-01 10:22:45 -0300

    add lynx project

---
+++ tot-lynx/src/App.tsx
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useState } from '@lynx-js/react';
+
+import './App.css';
+import arrow from './assets/arrow.png';
+import lynxLogo from './assets/lynx-logo.png';
+import reactLynxLogo from './assets/react-logo.png';
+
+export function App(props: {
+  onMounted?: () => void;
+}) {
+  const [alterLogo, setAlterLogo] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    console.info('Hello, ReactLynx');
+    props.onMounted?.();
+  }, []);
+
+  const onTap = useCallback(() => {
+    'background only';
+    setAlterLogo(!alterLogo);
+  }, [alterLogo]);
+
+  const handleClick = () => {
+    console.info('Button clicked');
+    setIsVisible(false);
+  };
+
+  return (
+    <view>
+      <view className="Background" />
+      <view className="App">
+        <view className="Banner">
+          <view className="Logo" bindtap={onTap}>
+            {alterLogo ? (
+              <image src={reactLynxLogo} className="Logo--react" />
+            ) : (
+              <image src={lynxLogo} className="Logo--lynx" />
+            )}
+          </view>
+          {isVisible && <text className="Title">The One Thing</text>}
+        </view>
+        <view className="Content">
+          <view className="Button" bindtap={handleClick}>
+            <text>Click me!</text>
+          </view>
+        </view>
+        <view style={{ flex: 1 }} />
+      </view>
+    </view>
+  );
+}
